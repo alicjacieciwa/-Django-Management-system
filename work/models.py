5ef5ba52f406470c7e8a1d3eb5434c24abb7e4de
Alicja Cięciwa <alicja.cieciwa@gmail.com>
2020-11-14 17:40:14 +0100

    logout button and user in navbar

--- work/models.py
+++ work/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 import datetime
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class HoursInput(models.Model):
     date = models.DateField(default=datetime.date.today)
-    hours_number = models.IntegerField()
+    hours_number = models.IntegerField(validators=[
+        MaxValueValidator(15, "Niemożliwe, że tyle pracowałeś"),
+        MinValueValidator(0, "Wprowadź liczbę dodatnią 0-15")
+    ])
     workplace = models.CharField(max_length=100)
 
